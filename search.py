decc6dc73b594a50b9f9a278b19a2e179c6804ef
Saad Mazhar <saad.maz2000@gmail.com>
2021-02-10 15:55:16 -0500

    minor change to dfs

--- search.py
+++ search.py
@@ -2,15 +2,16 @@ def check_neighbors(currentState, fringe, prev):
     print('my sack')
 
 
-def dfs(startState, goalState):
+def dfs(start_state, goal_state):
 
     fringe = []
     closed_set = set()
     prev = {}
+    fringe.append(start_state)
 
     while fringe:
         current_state = fringe.pop()
-        if current_state == goalState:
+        if current_state == goal_state:
             return "Success!", current_state, prev
         check_neighbors(current_state, fringe, prev)
         closed_set.append(current_state)
