2b40a92fe8e4476e39edfb83f92eb090445c777e
Saad Mazhar <saad.maz2000@gmail.com>
2021-02-13 03:21:05 -0500

    added a star

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import image
 import mazes
 import search
+import numpy as np
 
-maze = mazes.maze(15, 0.3)
+maze = mazes.maze(15, 0.2)
 start_state = (0, 0)
 goal_state = (len(maze)-1, len(maze)-1)
-image.show_maze(maze)
-result = search.bfs(maze, start_state, goal_state)
+result = search.a_star(maze, start_state, goal_state)
 if result:
     image.show_maze(maze)
-
+print(result)
--- search.py
+++ search.py
@@ -1,3 +1,8 @@
+from queue import PriorityQueue
+import numpy as np
+
+
+# method to check the neighbors of a state within the maze
 def check_neighbors(maze, current_state, fringe, prev, closed_set):
     # unpacks tuple
     (x, y) = current_state
@@ -20,6 +25,7 @@ def check_neighbors(maze, current_state, fringe, prev, closed_set):
         prev[((x + 1), y)] = current_state
 
 
+# method to implement DFS, just checks to see if the maze is solvable
 def dfs(maze, start_state, goal_state):
     # stack fringe of tuples implemented with a list
     fringe = []
@@ -46,6 +52,7 @@ def dfs(maze, start_state, goal_state):
     return False
 
 
+# method to implement BFS, checks if the maze is solvable and if it is it will write that path into the maze
 def bfs(maze, start_state, goal_state):
     # queue fringe of tuples implemented with a list
     fringe = []
@@ -64,6 +71,7 @@ def bfs(maze, start_state, goal_state):
         # FIFO list
         current_state = fringe.pop(0)
         if current_state == goal_state:
+            # get the path that leads to the goal state
             final_path = get_path(prev, start_state, goal_state)
             # trace the path in the maze
             trace_path(final_path, maze)
@@ -73,6 +81,46 @@ def bfs(maze, start_state, goal_state):
         closed_set.add(current_state)
 
     return False
+
+
+# method that implements A* and uses the euclidean distance heuristic to prioritize in the queue. If the maze is
+# solvable then it will also write the path into the maze.
+def a_star(maze, start_state, goal_state):
+    # priority queue fringe of tuples implemented with a list
+    fringe = PriorityQueue()
+
+    # dictionary of tuples that have been checked
+    closed_set = set()
+
+    # dictionary of tuples (x,y) coordinates that are the proper path
+    prev = {}
+
+    # first item in queue is the start
+    fringe.put((0, (0, start_state)))
+
+    while not fringe.empty():
+
+        # priority queue
+        (priority, (step, current_state)) = fringe.get()
+        if current_state == goal_state:
+            # get the path that leads to the goal state
+            final_path = get_path(prev, start_state, goal_state)
+            # trace the path in the maze
+            trace_path(final_path, maze)
+            return True
+        # check all the neighbors of current_state and add those to the fringe that are valid
+        check_neighbors_heuristic(maze, current_state, fringe, prev, closed_set, step, goal_state)
+        closed_set.add(current_state)
+
+    return False
+
+
+# method to retrieve euclidean distance
+def get_distance(start_state, goal_state):
+    # unpack tuples
+    (x, y) = start_state
+    (i, j) = goal_state
+    return np.sqrt((((i - x) ** 2) + ((j - y) ** 2)))
 
 
 # method to trace prev and get the path that will lead to the goal state
@@ -88,7 +136,6 @@ def get_path(prev, start_state, goal_state):
 
 # method to trace path within the maze
 def trace_path(path, maze):
-
     for pair in path:
         # tuple unpacking to get coordinates
         (x, y) = pair
@@ -97,3 +144,40 @@ def trace_path(path, maze):
         # set the path with a 3
         maze[x, y] = 3
 
+
+# method to check the neighbors of a state within the maze
+def check_neighbors_heuristic(maze, current_state, fringe, prev, closed_set, step, goal_state):
+    # unpacks tuple
+    (x, y) = current_state
+
+    # counts how many neighbors have been visited then subtracts that from the step
+
+    # Checks Top Neighboring cell, and adds to
+    if x - 1 >= 0 and maze[x - 1, y] != 1 and (x - 1, y) not in closed_set:
+        c_step = step + 1
+        total_distance = get_total_distance(c_step, (x - 1, y), goal_state)
+        fringe.put((total_distance, (c_step, (x - 1, y))))
+        prev[(x - 1, y)] = current_state
+    # Checks right Neighboring cell, and adds to
+    if y + 1 < len(maze) and maze[x, y + 1] != 1 and (x, y + 1) not in closed_set:
+        c_step = step + 1
+        total_distance = get_total_distance(c_step, (x, y + 1), goal_state)
+        fringe.put((total_distance, (c_step, (x, y + 1))))
+        prev[(x, y + 1)] = current_state
+    # Checks left Neighboring cell, and adds to
+    if y - 1 >= 0 and maze[x, y - 1] != 1 and (x, y - 1) not in closed_set:
+        c_step = step + 1
+        total_distance = get_total_distance(c_step, (x, y - 1), goal_state)
+        fringe.put((total_distance, (c_step, (x, y - 1))))
+        prev[(x, y - 1)] = current_state
+    # Checks bottom Neighboring cell, and adds to
+    if x + 1 < len(maze) and maze[x + 1, y] != 1 and (x + 1, y) not in closed_set:
+        c_step = step + 1
+        total_distance = get_total_distance(c_step, (x + 1, y), goal_state)
+        fringe.put((total_distance, (c_step, (x + 1, y))))
+        prev[(x + 1, y)] = current_state
+
+
+# gets the total distance from start to current node to goal, this is the priority for the queue
+def get_total_distance(step, neighbor, goal_state):
+    return step + get_distance(neighbor, goal_state)
