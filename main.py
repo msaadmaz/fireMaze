cb3a7f22ac8e4a5a66a236f61f05bf329465730c
Saad Mazhar <saad.maz2000@gmail.com>
2021-02-16 21:08:11 -0500

    fixed up advanced fire check neighbors and some method names

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import image
 import mazes
 import search
 
-maze = mazes.maze(100, 0.2)
+maze = mazes.create_maze(100, 0.2)
 start_state = (0, 0)
 goal_state = (len(maze)-1, len(maze)-1)
 result = search.bfs(maze, start_state, goal_state)
